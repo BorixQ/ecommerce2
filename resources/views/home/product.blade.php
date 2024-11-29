407e4fd2b18515f22c93cce14c4a37b018f9ce37
ginoliza <ginoangelolr@gmail.com>
2024-11-29 07:52:51 -0500

    add product detail page

--- resources/views/home/product.blade.php
+++ resources/views/home/product.blade.php
@@ -9,17 +9,21 @@
       @foreach($product as $products)
       <div class="col-sm-6 col-md-4 col-lg-3">
         <div class="box">
-          <a href="">
-            <div class="img-box">
-              <img src="products/{{$products->image}}" alt="">
-            </div>
-            <div class="detail-box">
-              <h6>{{$products->title}}</h6>
-              <h6>Price
-                <span>{{$products->price}}</span>
-              </h6>
-            </div>
-          </a>
+
+          <div class="img-box">
+            <img src="products/{{$products->image}}" alt="">
+          </div>
+          <div class="detail-box">
+            <h6>{{$products->title}}</h6>
+            <h6>Price
+              <span>{{$products->price}}</span>
+            </h6>
+          </div>
+
+          <div style="padding: 15px;">
+            <a href="{{url('product_details', $products->id)}}" class="btn btn-danger">Details</a>
+          </div>
+
         </div>
       </div>
       @endforeach
